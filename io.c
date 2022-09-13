a97369fcbb8b2f7f959c8578f26e170c4e09fdc7
Rafii271 <rafifranciska271@gmail.com>
2022-09-14 00:30:16 +0700

    feat: first commit

---
+++ io.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char chrtest[10];
+    printf ("Enter ur Name: ");
+    scanf("%s", chrtest);
+    printf("Name is: %s\n",chrtest);
+    return 0;
+}
